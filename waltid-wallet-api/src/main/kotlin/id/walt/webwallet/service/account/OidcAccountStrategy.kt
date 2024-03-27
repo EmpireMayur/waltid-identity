83bd1c69c1979570517ca65e278dfe4daeeeb773
SuperBatata <khaled.battiche@esprit.tn>
2024-03-27 03:23:22 +0100

    fix: get user info based on oidc provider

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/account/OidcAccountStrategy.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/account/OidcAccountStrategy.kt
@@ -1,8 +1,11 @@
 package id.walt.webwallet.service.account
 
+import id.walt.webwallet.config.ConfigManager
+import id.walt.webwallet.config.OidcConfiguration
 import id.walt.webwallet.db.models.Accounts
 import id.walt.webwallet.db.models.OidcLogins
 import id.walt.webwallet.utils.JwkUtils.verifyToken
+import id.walt.webwallet.web.controllers.getUserInfo
 import id.walt.webwallet.web.model.OidcAccountRequest
 import kotlinx.datetime.Clock
 import kotlinx.datetime.toJavaInstant
@@ -14,17 +17,22 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object OidcAccountStrategy : PasswordlessAccountStrategy<OidcAccountRequest>() {
     override suspend fun register(tenant: String, request: OidcAccountRequest): Result<RegistrationResult> {
         val jwt = verifyToken(request.token)
+        val config = ConfigManager.getConfig<OidcConfiguration>()
 
-        if (AccountsService.hasAccountOidcId(jwt.subject)) {
-            throw IllegalArgumentException("Account already exists with OIDC id: ${request.token}")
+        require(!(AccountsService.hasAccountOidcId(jwt.subject))) { "Account already exists with OIDC id: ${request.token}" }
+
+        val userInfo = when (config.providerName == "auth0") {
+            true -> getUserInfo(request.token)
+            false -> jwt.claims
         }
+
 
         val createdAccountId = transaction {
             val accountId = Accounts.insert {
                 it[Accounts.tenant] = tenant
                 it[id] = UUID.generateUUID()
-                it[name] = jwt.getClaim("name").asString()
-                it[email] = jwt.getClaim("email").asString()
+                it[name] = userInfo["name"].toString()
+                it[email] = userInfo["email"].toString()
                 it[createdOn] = Clock.System.now().toJavaInstant()
             }[Accounts.id]
 
